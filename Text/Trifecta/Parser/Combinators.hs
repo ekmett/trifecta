fc1a2f4fbb20bbb86aa1759faaa27d6003e539fd
James <wilcoxjay@gmail.com>
2011-08-19 00:08:04 -0700

    notChar and manyTill docs

--- Text/Trifecta/Parser/Combinators.hs
+++ Text/Trifecta/Parser/Combinators.hs
@@ -180,7 +180,6 @@ chainr1 p op = scan where
 --
 --    Note the overlapping parsers @anyChar@ and @string \"-->\"@, and
 --    therefore the use of the 'try' combinator.
--- manyTill :: Alternative m => m a -> m end -> m [a]
 manyTill :: (Alternative m, MonadPlus m) => m a -> m end -> m [a]
 {-
 manyTill p end = scan
