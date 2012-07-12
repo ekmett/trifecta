30e71d2818ffb9024fb541eb69d3c2f0d7d0da99
Edward Kmett <ekmett@gmail.com>
2012-07-12 16:49:59 -0400

    README, example fixes

---
+++ README.markdown
@@ -0,0 +1,15 @@
+trifecta
+========
+
+[![Build Status](https://secure.travis-ci.org/ekmett/trifecta.png?branch=master)](http://travis-ci.org/ekmett/trifecta)
+
+This package provides a parser that focuses on nice diagnostics.
+
+Contact Information
+-------------------
+
+Contributions and bug reports are welcome!
+
+Please feel free to contact me through github or on the #haskell IRC channel on irc.freenode.net.
+
+-Edward Kmett
--- examples/RFC2616.hs
+++ examples/RFC2616.hs
@@ -5,13 +5,10 @@ import Control.Applicative
 import Control.Exception (bracket)
 import System.Environment (getArgs)
 import System.IO (hClose, openFile, IOMode(ReadMode))
-import Text.Trifecta.Parser.Class hiding (satisfy)
-import Text.Trifecta.Parser.Token
-import Text.Trifecta.Parser.ByteString
-import Text.Trifecta.Parser.Combinators
-import Text.Trifecta.Parser.Char8
-import Text.Trifecta.Highlight.Prim
-import qualified Text.Trifecta.ByteSet as S
+import Text.Trifecta hiding (token)
+import Text.Parser.Token.Highlight
+import Text.Parser.Token.Style
+import Data.CharSet.ByteSet as S
 import qualified Data.ByteString as B
 
 infixl 4 <$!>
@@ -21,13 +18,13 @@ f <$!> ma = do
   a <- ma
   return $! f a
 
-token :: MonadParser m => m Char
+token :: CharParsing m => m Char
 token = noneOf $ ['\0'..'\31'] ++ "()<>@,;:\\\"/[]?={} \t" ++ ['\128'..'\255']
 
 isHSpace :: Char -> Bool
 isHSpace c = c == ' ' || c == '\t'
 
-skipHSpaces :: MonadParser m => m ()
+skipHSpaces :: CharParsing m => m ()
 skipHSpaces = skipSome (satisfy isHSpace)
 
 data Request = Request {
@@ -36,17 +33,17 @@ data Request = Request {
     , requestProtocol :: String
     } deriving (Eq, Ord, Show)
 
-requestLine :: MonadParser m => m Request
+requestLine :: TokenParsing m => m Request
 requestLine = Request <$!> (highlight ReservedIdentifier (some token) <?> "request method")
-                       <*  skipHSpaces 
+                       <*  skipHSpaces
                        <*> (highlight Identifier (some (satisfy (not . isHSpace))) <?> "url")
-                       <*  skipHSpaces 
+                       <*  skipHSpaces
                        -- <*> try (highlight ReservedIdentifier (string "HTTP/" *> many httpVersion <* endOfLine) <?> "protocol")
                        <*> (try (highlight ReservedIdentifier (string "HTTP/" *> many httpVersion <* endOfLine)) <?> "protocol")
  where
   httpVersion = satisfy $ \c -> c == '1' || c == '0' || c == '.' || c == '9'
 
-endOfLine :: MonadParser m => m ()
+endOfLine :: CharParsing m => m ()
 endOfLine = (string "\r\n" *> pure ()) <|> (char '\n' *> pure ())
 
 data Header = Header {
@@ -54,14 +51,14 @@ data Header = Header {
     , headerValue :: [String]
     } deriving (Eq, Ord, Show)
 
-messageHeader :: MonadParser m => m Header
-messageHeader = (\h b c -> Header h (b : c)) 
+messageHeader :: TokenParsing m => m Header
+messageHeader = (\h b c -> Header h (b : c))
             <$!> (highlight ReservedIdentifier (some token)  <?> "header name")
              <*  highlight Operator (char ':') <* skipHSpaces
              <*> (highlight Identifier (manyTill anyChar endOfLine) <?> "header value")
              <*> (many (skipHSpaces *> manyTill anyChar endOfLine) <?> "blank line")
 
-request :: MonadParser m => m (Request, [Header])
+request :: TokenParsing m => m (Request, [Header])
 request = (,) <$> requestLine <*> many messageHeader <* endOfLine
 
 lumpy arg = do
